5121b46d37dd5c0e26d2117d497b2e0424b4d7c0
Muskan <Muskan>
2024-08-30 10:35:14 +0530

    added qualities and menu page

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,12 +1,19 @@
-import About from "../components/About"
-import HeroSection from "../components/HeroSection"
+import About from "../components/About";
+import HeroSection from "../components/HeroSection";
+import Menu from "../components/Menu";
+import Qualities from "../components/Qualities";
+import WhoAreWe from "../components/WhoAreWe";
 
 const Home = () => {
   return (
-    <><HeroSection/>
-    <About/>
+    <>
+      <HeroSection />
+      <About />
+      <Qualities />
+      <Menu/>
+      <WhoAreWe/>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
